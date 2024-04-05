50722ddfb30e3e3e0805b70ef465e13cfbdf994a
Emilio <emifont01@gmail.com>
2024-04-04 22:30:02 -0400

    feat: adds conversation controller

--- server/start/routes.ts
+++ server/start/routes.ts
@@ -8,7 +8,9 @@
 */
 
 const UsersController = () => import('#controllers/users_controller')
+const ConversationsController = () => import('#controllers/conversations_controller')
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
@@ -20,3 +22,12 @@ router.get('users', [UsersController, 'getUser'])
 router.post('users', [UsersController, 'signUp'])
 router.post('users', [UsersController, 'signOut'])
 router.post('users', [UsersController, 'signIn'])
+
+router.get('api/conversations', [ConversationsController, 'index']).use(middleware.auth())
+router
+  .post('api/conversations', [ConversationsController, 'createConversation'])
+  .use(middleware.auth())
+
+router
+  .post('api/conversations/:conversation_id/messages', [ConversationsController, 'createMessage'])
+  .use(middleware.auth())
