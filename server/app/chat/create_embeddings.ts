3fe0f6bfa34b1328117a10e431c0ec7dd106ef7b
Emilio <emifont01@gmail.com>
2024-06-09 17:35:11 -0400

    feat: starting to process pdf

--- server/app/chat/create_embeddings.ts
+++ server/app/chat/create_embeddings.ts
@@ -1,5 +1,7 @@
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { vectorStore } from './vector_stores/pinecone.ts'
+
 export async function createEmbeddingsForPdf(pdfId: string, pdfPath: string) {
   const textSplitter = new RecursiveCharacterTextSplitter({
     chunkSize: 500,
@@ -7,19 +9,5 @@ export async function createEmbeddingsForPdf(pdfId: string, pdfPath: string) {
   })
   const pdfLoader = new PDFLoader(pdfPath)
   const docs = await pdfLoader.loadAndSplit(textSplitter)
-  console.log(docs)
-
-  //     Generate and store embeddings for the given pdf
-  //
-  //     1. Extract text from the specified PDF.
-  //     2. Divide the extracted text into manageable chunks.
-  //     3. Generate an embedding for each chunk.
-  //     4. Persist the generated embeddings.
-  //
-  //     :param pdf_id: The unique identifier for the PDF.
-  //     :param pdf_path: The file path to the PDF.
-  //
-  //     Example Usage:
-  //
-  // create_embeddings_for_pdf('123456', '/path/to/pdf')
+  await vectorStore.addDocuments(docs)
 }
