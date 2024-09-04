4eeb2c9601be83bdefc4dd94383b198a2a51e45b
Emilio <emifont01@gmail.com>
2024-09-04 17:14:42 -0400

    chore: added vectorstore, embeddings and ai tools

---
+++ server2/src/chat/create_embeddings.ts
@@ -0,0 +1,14 @@
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { vectorStore } from './vector_stores/pinecone.ts'
+
+export async function createEmbeddingsForPdf(pdfId: string, pdfPath: string) {
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 500,
+        chunkOverlap: 100,
+    })
+    console.log(`Loading and splitting PDF ${pdfId} from ${pdfPath}`);
+    const pdfLoader = new PDFLoader(pdfPath);
+    const docs = await pdfLoader.loadAndSplit(textSplitter);
+    await vectorStore.addDocuments(docs);
+}
